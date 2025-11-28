6ab86b695999e3c0bb8e5deb51cc8fbf8a7d25a5
Illia Karcheuski <your.email@example.com>
2025-11-28 22:40:44 +0300

    hw 20 done

--- utils/file.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def abs_path_from_project(relative_path: str):
-    return os.path.abspath(
-        os.path.join(os.path.dirname(__file__), '../..', relative_path)
-    )
-
-def path_from_project(relative_path: str):
-    return os.path.join(os.path.dirname(__file__), '../..', relative_path)
